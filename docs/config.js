af4cd85b88c04c00ebee187789c9c4a63e2e39a2
X66666hj <xhjzshuai@gmail.com>
2026-01-30 13:13:45 +0800

    chore(annotator): update google script url (working)

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyHjnfQWGUaSQSeapL_kURk5shk-8yZDElKYl-u8d8RBSBUKqWHIcnvvxC-S6Jowg6zpg/exec",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyvWDtGtFJbzYn0V8fljCropONRj5cB5s0CRwOw1VDu0zCH4GdYxhIRtX5PMT15Bl8pEg/exec",
 };
